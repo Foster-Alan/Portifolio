478bcf29cc9915fcb108123426c5df9da6b74408
Alan Foster <alan-af@outlook.com>
2022-11-07 18:54:51 -0300

    test

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,13 +9,15 @@ export default class Projects extends Component {
         <h1>Projects</h1>
         <a target="blank" href="https://foster-pixel-art.netlify.app">Pixel Art</a>
         <br />
-        <a target="blank" href="https://foster-trivia.netlify.app">Trivia</a>
+        <a target="blank" href="https://foster-trivia.netlify.app">Trunfo</a>
         <br />
         <a target="blank" href="https://foster-wallet.netlify.app">Wallet</a>
         <br />
         <a target="blank" href="https://foster-tunes.netlify.app">Tunes</a>
         <br />
         <a target="blank" href="https://foster-starwars.netlify.app">StarWars</a>
+        <br />
+        <a target="blank" href="https://foster-trivia.netlify.app">Trivia</a>
       </div>
     )
   }
