b385138a81630b784f79d826afb72203fb232d6b
Alan Foster <alan-af@outlook.com>
2022-11-16 13:19:08 -0300

    fix

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,7 +8,6 @@ import trunfo from '../img/trunfo.png'
 import wallet from '../img/wallet.png'
 import tunes from '../img/tunes.png'
 
-//test 1
 export default class Projects extends Component {
   render() {
     return (
