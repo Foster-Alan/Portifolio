8115bc63a822c5ac8b788cd893e4211807fcd93a
Alan Foster <alan-af@outlook.com>
2023-08-09 11:53:50 -0300

    ADD - logo

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,12 +4,15 @@ import "../Styles/Header.css";
 import { AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 
+import logo01 from '../img/logo01.png'
+
 export default function Header() {
   const [menu, setMenu] = useState(true);
   return (
     <header>
       <nav className="header-conteiner">
-       
+
+        <img src={logo01} alt="Logo Foster" />
         <Link to="/">Início</Link>
         <Link to="/About">Sobre</Link>
         <Link to="/skills">Skills</Link>
@@ -20,10 +23,13 @@ export default function Header() {
       </nav>
       <div className="header-mobile">
         {menu ? (
+          <div>
+          <img src={logo01} alt="Logo Foster" />
           <GiHamburgerMenu size={25}
             className="btn-menu"
             onClick={() => setMenu(!menu)}
           />
+          </div>
         ) : (
           <AiOutlineClose size={25} className="btn-menu" onClick={() => setMenu(!menu)} />
         )}
